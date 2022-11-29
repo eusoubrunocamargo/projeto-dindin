f40b63e635e405d1b430911943be7a979e6ac103
Laezio Picanço <laeziop28@hotmail.com>
2022-11-29 17:42:44 -0300

    feature/config-axios: commit inicial da configuração do axios

--- dindin/src/SignIn/index.js
+++ dindin/src/SignIn/index.js
@@ -1,19 +1,19 @@
-import './styles.css';
+import "./styles.css";
+import api from "../services/api";
 
 function SignIn() {
-
   return (
-    <div className='container-geral'>
-      <form className='container-form'>
+    <div className="container-geral">
+      <form className="container-form">
         <h1>Cadastre-se</h1>
-        <input type='text' placeholder='Nome'></input>
-        <input type='text' placeholder='E-mail'></input>
-        <input type='text' placeholder='Senha'></input>
-        <input type='text' placeholder='Confirme sua senha'></input>
-        <button>Cadastrar</button>
+        <input type="text" placeholder="Nome" required></input>
+        <input type="email" placeholder="E-mail"></input>
+        <input type="password" placeholder="Senha"></input>
+        <input type="password" placeholder="Confirme sua senha"></input>
+        <button type="submit">Cadastrar</button>
         <span>Já tem cadastro? Faça login!</span>
       </form>
-    </div >
+    </div>
   );
 }
 
--- dindin/src/index.js
+++ dindin/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import Main from './Main/';
-// import SignIn from './SignIn';
-import Dashboard from './Dashboard';
+import SignIn from './SignIn';
+// import Dashboard from './Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Main /> */}
-    {/* <SignIn /> */}
-    <Dashboard />
+    <SignIn />
+    {/* <Dashboard /> */}
   </React.StrictMode>
 );
 
