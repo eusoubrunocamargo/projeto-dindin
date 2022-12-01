d82533ab7364028c12e6eb15a61e4ebe473388be
Bruno Camargo <eusoubrunocamargo@gmail.com>
2022-11-30 21:59:52 -0300

    btn editar registro

--- dindin/src/components/Extrato/index.js
+++ dindin/src/components/Extrato/index.js
@@ -1,6 +1,8 @@
 import '../Extrato/styles.css'
 import Lapis from '../../assets/lapis.svg';
 import Lixeira from '../../assets/lixeira.svg';
+import { useEffect, useState } from 'react';
+import ModalDelete from '../ModalDeleteRegistro';
 
 export default function Extrato() {
 
@@ -11,7 +13,7 @@ export default function Extrato() {
             dia: "Segunda",
             descricao: "Barzinho",
             categoria: "Lazer",
-            valor: "R$100,00"
+            valor: "R$100,00",
         },
         {
             id: 2,
@@ -19,9 +21,31 @@ export default function Extrato() {
             dia: "Quarta",
             descricao: "Barzinho",
             categoria: "Lazer",
-            valor: "R$100,00"
+            valor: "R$100,00",
         },
-    ]
+    ];
+
+    const [index, setIndex] = useState();
+    const [deleteItem, setDeleteItem] = useState([]);
+
+    useEffect(() => {
+
+        if (!index) {
+            return;
+        }
+        console.log(index);
+        setDeleteItem([...deleteItem, index]);
+        console.log(deleteItem);
+
+    }, [index]);
+
+    useEffect(() => {
+
+
+
+    }, [deleteItem]);
+
+
 
     return (
 
@@ -35,13 +59,13 @@ export default function Extrato() {
                         <li>Descrição</li>
                         <li>Categoria</li>
                         <li>Valor</li>
+                        <li></li>
                     </ul>
                 </div>
 
-
                 {registros.map((item) => {
                     return (
-                        <div key={item.id} className='container-item'>
+                        <div key={item.id} className='container-item' id='container-item'>
                             <div key={item.id} className='container-itens-extrato'>
                                 <ul>
                                     <li>{item.data}</li>
@@ -49,11 +73,18 @@ export default function Extrato() {
                                     <li>{item.descricao}</li>
                                     <li>{item.categoria}</li>
                                     <li>{item.valor}</li>
+                                    <li>
+                                        <div className='container-editar-item'>
+                                            {deleteItem.includes(item.id) ? <ModalDelete /> : null}
+                                            <button><img src={Lapis} alt='Lapis' /></button>
+                                            <button onClick={() => {
+                                                if (deleteItem.includes)
+                                                    setDeleteItem([...deleteItem, item.id]);
+
+                                            }}><img src={Lixeira} alt='Lixeira' /></button>
+                                        </div>
+                                    </li>
                                 </ul>
-                            </div>
-                            <div className='container-editar-item'>
-                                <button><img src={Lapis} alt='Lapis' /></button>
-                                <button><img src={Lixeira} alt='Lixeira' /></button>
                             </div>
                         </div>
                     )
---
+++ dindin/src/components/ModalDeleteRegistro/index.js
@@ -0,0 +1,13 @@
+import '../ModalDeleteRegistro/styles.css'
+
+export default function ModalDelete() {
+
+
+
+    return (
+        <>
+            <div>Teste...</div>
+
+        </>
+    )
+}
